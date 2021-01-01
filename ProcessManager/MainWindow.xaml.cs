918e66c263d7ef8440b61e7cab7f8cf88f3352b7
iiAlosha <arma.55@outlook.com>
2021-01-01 16:00:40 +0300

    Proje dosyası ekle.

---
+++ ProcessManager/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace ProcessManager
+{
+    /// <summary>
+    /// MainWindow.xaml etkileşim mantığı
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = false;
+            openFileDialog.Filter = "Alosha Files (*.alosha)|*.alosha";
+            if (openFileDialog.ShowDialog() == true)
+            {
+
+            }
+        }
+    }
+}
---
+++ ProcessManager/Managers/ProcessManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessManager.Managers
+{
+    public static class ProcessManager
+    {
+        public static List<Process> ActiveProcess;
+        public static List<string> FileNames;
+        public static Process[] GetAllProcesses() => Process.GetProcesses();
+
+
+        public static Process StartProcess(ProcessStartInfo info)
+        {
+            var process = Process.Start(info);
+            ActiveProcess.Add(process);
+            FileNames.Add(info.FileName);
+            return process;
+        }
+    }
+}
