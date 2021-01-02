f7dbd5a4e24eddc3b75aeb4eb3be2ce5ec510096
iiAlosha <arma.55@outlook.com>
2021-01-02 19:09:17 +0300

    update

--- ProcessManager/Managers/ProcessManager.cs
+++ ProcessManager/Managers/ProcessManager.cs
@@ -3,6 +3,7 @@ using System.CodeDom;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Management;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,17 +11,58 @@ namespace ProcessManager.Managers
 {
     public static class ProcessManager
     {
-        public static List<Process> ActiveProcess;
-        public static List<string> FileNames;
-        public static Process[] GetAllProcesses() => Process.GetProcesses();
+        public static List<Process> Processes = new List<Process>();
 
-
-        public static Process StartProcess(ProcessStartInfo info)
+        public static Process StartProcess(string path, string command)
         {
-            var process = Process.Start(info);
-            ActiveProcess.Add(process);
-            FileNames.Add(info.FileName);
+            var process = new Process();
+            process.StartInfo = new ProcessStartInfo()
+            {
+                WorkingDirectory = path,
+                // CreateNoWindow = true,
+                // WindowStyle = ProcessWindowStyle.Hidden,
+                Arguments = $"cmd /K {command}",
+                FileName = "cmd.exe",
+                UseShellExecute = false
+            };
+            process.ErrorDataReceived += ProcessOnErrorDataReceived;
+            process.OutputDataReceived += ProcessOnOutputDataReceived;
+            Processes.Add(process);
+            process.Start();
             return process;
+        }
+
+        public static void KillProcessAndChildrens(int pid)
+        {
+            ManagementObjectSearcher processSearcher = new ManagementObjectSearcher
+                ("Select * From Win32_Process Where ParentProcessID=" + pid);
+            ManagementObjectCollection processCollection = processSearcher.Get();
+
+            if (processCollection != null)
+            {
+                foreach (ManagementObject mo in processCollection) KillProcessAndChildrens(Convert.ToInt32(mo["ProcessID"]));
+            }
+
+            try
+            {
+                Process proc = Process.GetProcessById(pid);
+                if (!proc.HasExited) proc.Kill();
+            }
+            catch (ArgumentException)
+            {
+                // Process already exited.
+            }
+        }
+
+
+        private static void ProcessOnOutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Console.WriteLine("OUTPUT: " + e.Data);
+        }
+
+        private static void ProcessOnErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Console.WriteLine("ERROR: " + e.Data);
         }
     }
 }
---
+++ ProcessManager/Windows/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Management;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+using Microsoft.Win32;
+using ProcessManager.Modules;
+using Application = System.Windows.Application;
+using ListBox = System.Windows.Controls.ListBox;
+using MouseEventArgs = System.Windows.Input.MouseEventArgs;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
+
+namespace ProcessManager
+{
+    /// <summary>
+    /// MainWindow.xaml etkileşim mantığı
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UIElement_OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void HomeButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void InformationButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void ExitButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var processes = Managers.ProcessManager.Processes;
+            foreach (var process in processes)
+            {
+                Managers.ProcessManager.KillProcessAndChildrens(process.Id);
+            }
+            Application.Current.Shutdown();
+        }
+    }
+}
